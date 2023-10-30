d0202026bf2678ae8506881484eb2d8cb5e10974
sthallysson <sthallysson5@gmail.com>
2023-10-29 21:58:13 -0300

    fix: ajusted datas

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -5,20 +5,20 @@ const DUMMY_USERS = {
       id: '1',
       name: 'Maria',
       email: 'maria@alunos.com',
-      avatarUrl: "https://i.pravatar.cc/"),
+      avatarUrl: "https://i.pravatar.cc/250?img=16"),
   '2': User(
       id: '2',
       name: 'João',
       email: 'joaogomes@hotmail.com',
-      avatarUrl: "https://i.pravatar.cc/"),
+      avatarUrl: "https://i.pravatar.cc/250?img=17"),
   '3': User(
       id: '3',
       name: 'Rafael',
       email: 'rafael@gmail.com',
-      avatarUrl: "https://i.pravatar.cc/"),
+      avatarUrl: "https://i.pravatar.cc/250?img=15"),
   '4': User(
       id: '4',
       name: 'Joana',
       email: 'joanadarc@gmail.com',
-      avatarUrl: "https://i.pravatar.cc/"),
+      avatarUrl: "https://i.pravatar.cc/250?img=5"),
 };
