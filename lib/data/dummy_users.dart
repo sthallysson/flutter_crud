21dbc6986faa48ff0f3b1ec09c671b9efb47b002
sthallysson <sthallysson5@gmail.com>
2023-10-29 21:13:25 -0300

    feat: mocked datas

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: unnecessary_const
+
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com',
+      avatarUrl: "https://i.pravatar.cc/"),
+  '2': const User(
+      id: '2',
+      name: 'João',
+      email: 'joaogomes@hotmail.com',
+      avatarUrl: "https://i.pravatar.cc/"),
+  '3': const User(
+      id: '3',
+      name: 'Rafael',
+      email: 'rafael@gmail.com',
+      avatarUrl: "https://i.pravatar.cc/"),
+  '4': const User(
+      id: '4',
+      name: 'Joana',
+      email: 'joanadarc@gmail.com',
+      avatarUrl: "https://i.pravatar.cc/"),
+};
